e54f49ebfb4d12fd52adc7efe852645a553c1f39
Aneta Jastrzębska <1544710+anetaj@users.noreply.github.com>
2024-09-21 19:19:26 +0200

    threads history list

---
+++ extensions/void/src/sidebar/components/ThreadHistory.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { ChatThread } from "../../shared_types"
+import { useChat } from "../context"
+
+const ThreadHistory = ({
+	threads,
+}: {
+	threads: ChatThread[]
+}) => {
+	const { selectThread } = useChat()
+
+	return (
+		<div className="flex flex-col space-y-1 mt-2">
+			{threads.map((thread) => (
+				<button
+					key={thread.id}
+					className="btn btn-secondary btn-sm"
+					onClick={() => selectThread(thread)}
+				>
+					{new Date(thread.createdAt).toLocaleString()}
+				</button>
+			))}
+		</div>
+	)
+}
+
+export default ThreadHistory

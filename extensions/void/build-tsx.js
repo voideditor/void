7072933a88b0b73b951686311815a98d104993d7
Andrew Pareles <43356051+andrewpareles@users.noreply.github.com>
2024-10-20 19:20:03 -0700

    Update build-tsx.js

--- extensions/void/build-tsx.js
+++ extensions/void/build-tsx.js
@@ -5,13 +5,6 @@ esbuild.build({
   entryPoints: ['src/sidebar/index.tsx'],
   bundle: true,
   minify: true,
-	loader: {
-    '.ttf': 'file',
-    '.woff': 'file',
-    '.woff2': 'file',
-    '.eot': 'file',
-    '.svg': 'file',
-  },
   sourcemap: true,
   outfile: 'dist/sidebar/index.js',
   format: 'iife', // apparently iife is safe for browsers (safer than cjs)

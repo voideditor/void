8b9a9c7e2b0ac182ec1fe7b113c9d127105bde35
Andrew Pareles <andrewpareles@gmail.com>
2024-11-11 03:39:18 -0800

    prefix

--- src/vs/workbench/contrib/void/browser/react/build.js
+++ src/vs/workbench/contrib/void/browser/react/build.js
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 execSync('npx rimraf out/ && npx rimraf src2/')
 
 // build and scope tailwind
-execSync('npx scope-tailwind ./src -o src2/ -s void-scope -c styles.css')
+execSync('npx scope-tailwind ./src -o src2/ -s void-scope -c styles.css -p "prefix-" ')
 
 // tsup to build src2/ into out/
 execSync('tsup')

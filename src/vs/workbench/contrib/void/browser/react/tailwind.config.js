f8f60ef6b08993910a887f5e4d20c779a6db898e
Andrew Pareles <andrewpareles@gmail.com>
2024-11-11 03:38:21 -0800

    re-add tailwind for content:['./src2'] tag

---
+++ src/vs/workbench/contrib/void/browser/react/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src2/**/*.{jsx,tsx}'], // uses these files to decide how to transform the css file
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	prefix: 'prefix-'
+}
+

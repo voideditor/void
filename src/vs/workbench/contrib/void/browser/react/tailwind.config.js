f713a624f9f59d4a2ca68096deb6135479bce5bb
Andrew Pareles <andrewpareles@gmail.com>
2024-11-11 03:29:50 -0800

    add prefix in tailwind.config, apparently adding it in scope-tailwind doesnt work

--- src/vs/workbench/contrib/void/browser/react/tailwind.config.js
+++ src/vs/workbench/contrib/void/browser/react/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{js,ts,tsx}'], // uses these files to decide how to transform the css file
+	content: ['./src2/**/*.{jsx,tsx}'], // uses these files to decide how to transform the css file
 	theme: {
 		extend: {},
 	},
 	plugins: [],
+	prefix: 'prefix-'
 }
 

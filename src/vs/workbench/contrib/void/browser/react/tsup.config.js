bd49aaffde2baa8ee468330f95f3941046ff992b
Andrew Pareles <andrewpareles@gmail.com>
2024-11-14 01:06:15 -0800

    update noExternal

--- src/vs/workbench/contrib/void/browser/react/tsup.config.js
+++ src/vs/workbench/contrib/void/browser/react/tsup.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: [
 		'./src2/sidebar-tsx/Sidebar.tsx',
-		'./src2/util/sendLLMMessage.ts',
-		'./src2/util/posthog.ts',
+		'./src2/util/sendLLMMessage.tsx',
+		'./src2/util/posthog.tsx',
 	],
 	outDir: './out',
 	format: ['esm'],
@@ -17,7 +17,15 @@ export default defineConfig({
 	injectStyle: true, // bundle css into the output file
 	outExtension: () => ({ js: '.js' }),
 	// default behavior is to take local files and make them internal (bundle them) and take imports like 'react' and leave them external (don't bundle them), we want the opposite in many ways
-	noExternal: ['react', 'react-dom'], // noExternal means we should take these things and make them not external (bundle them into the output file)
+	noExternal: [ // noExternal means we should take these things and make them not external (bundle them into the output file)
+		'react',
+		'react-dom',
+		'posthog-js',
+		'@google/generative-ai',
+		'ollama',
+		'@anthropic-ai/sdk',
+		'openai',
+	],
 	external: [ // these imports should be kept external ../../../ are external (this is just an optimization so the output file doesn't re-implement functions)
 		new RegExp('../../../*'
 			.replaceAll('.', '\\.')

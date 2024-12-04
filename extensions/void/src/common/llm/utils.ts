f2b176dfdab71df9e73ede09cd8a0c90993b14ae
Andrew Pareles <andrewpareles@gmail.com>
2024-12-04 15:12:45 -0800

    remove `extensions/` code

--- extensions/void/src/common/llm/utils.ts
+++
@@ -1,6 +0,0 @@
-export const parseMaxTokensStr = (maxTokensStr: string) => {
-	let int = isNaN(Number(maxTokensStr)) ? undefined : parseInt(maxTokensStr)
-	if (Number.isNaN(int))
-		return undefined
-	return int
-}

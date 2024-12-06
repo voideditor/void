4ba00f62160caabb7f66c4543a8bdc571eee7668
Andrew Pareles <andrewpareles@gmail.com>
2024-12-06 02:55:28 -0800

    scope-tailwind doc

--- src/vs/workbench/contrib/void/browser/react/build.js
+++ src/vs/workbench/contrib/void/browser/react/build.js
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 // clear temp dirs
 execSync('npx rimraf out/ && npx rimraf src2/')
 
-// build and scope tailwind
+// build and scope tailwind: https://www.npmjs.com/package/scope-tailwind
 execSync('npx scope-tailwind ./src -o src2/ -s void-scope -c styles.css -p "prefix-" ')
 
 // tsup to build src2/ into out/

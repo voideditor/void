0c7898af0305139adbc6a3a8f5bf316fbf0c179d
Jérôme Commaret <jcommaret@gmail.com>
2024-12-10 15:42:38 +0100

    Remove system message filter

--- src/vs/workbench/contrib/void/browser/react/src/sendLLMMessage/mistral.tsx
+++ src/vs/workbench/contrib/void/browser/react/src/sendLLMMessage/mistral.tsx
@@ -1,32 +1,19 @@
 import { Mistral } from "@mistralai/mistralai";
 import { SendLLMMessageFnTypeInternal } from "./_types.js";
-import { parseMaxTokensStr } from "../../../registerConfig.js";
 
 type LLMMessageMistral = {
 	role: "user" | "assistant" | "system";
 	content: string;
 };
 
-export const sendMistralMsg: SendLLMMessageFnTypeInternal = ({
-	messages,
-	onText,
-	onFinalMessage,
-	onError,
-	voidConfig,
-	_setAborter,
-}) => {
+export const sendMistralMsg: SendLLMMessageFnTypeInternal = ({ messages, onText, onFinalMessage, onError, voidConfig, _setAborter,}) => {
 	let fullText = "";
+
 	const thisConfig = voidConfig.mistral;
 
 	const mistral = new Mistral({
 		apiKey: thisConfig.apikey,
 	});
-
-	// Find system messages
-	const systemMessage = messages
-		.filter((msg) => msg.role === "system")
-		.map((msg) => msg.content)
-		.join("\n");
 
 	// Filtrer les messages non-système
 	const mistralMessages = messages
@@ -36,16 +23,13 @@ export const sendMistralMsg: SendLLMMessageFnTypeInternal = ({
 			content: msg.content,
 		})) as LLMMessageMistral[];
 
-	mistral.chat.create({
-		systemMessage:systemMessage,
+	mistral.chat.stream({
 		messages: mistralMessages,
 		model: thisConfig.model,
 		stream: true,
-		max_tokens: parseMaxTokensStr(voidConfig.default.maxTokens),
 	})
-	.then(async (response) => {
-		_setAborter(() => response.controller.abort());
 
+	.then(async (response) => {
 		for await (const chunk of response) {
 			const newText = chunk.choices[0]?.delta?.content || "";
 			fullText += newText;
@@ -53,8 +37,9 @@ export const sendMistralMsg: SendLLMMessageFnTypeInternal = ({
 		}
 		onFinalMessage({ fullText });
 	})
+
 	.catch((error) => {
-		if (error instanceof Mistral.APIError && error.status === 401) {
+		if (error.status === 401) {
 			onError({ error: "Invalid API key." });
 		} else {
 			onError({ error });

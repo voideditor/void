14e314e5b86bb08cda9e809e3e27a73be82ab33e
Andrew Pareles <andrewpareles@gmail.com>
2024-12-14 18:03:50 -0800

    update contrib

--- src/vs/workbench/browser/parts/panel/panelActions.ts
+++ src/vs/workbench/browser/parts/panel/panelActions.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-//
+
 import './media/panelpart.css';
 import { localize, localize2 } from '../../../../nls.js';
 import { KeyMod, KeyCode } from '../../../../base/common/keyCodes.js';

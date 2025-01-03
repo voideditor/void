ffcd41255c00e567bc2e13d46847053bbd6a6b6f
Andrew Pareles <andrewpareles@gmail.com>
2025-01-02 19:55:23 -0800

    C

--- src/vs/platform/void/common/metricsService.ts
+++ src/vs/platform/void/common/metricsService.ts
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------------------------------------
- *  Copyright (c) 2025 Glass Devtools, Inc. All rights reserved.
+ *  Copyright (C) 2025 Glass Devtools, Inc. All rights reserved.
  *  Void Editor licensed under the AGPL 3.0 License. See LICENSE.txt in the project root for more information.
  *-----------------------------------------------------------------------------------------------------------*/
 

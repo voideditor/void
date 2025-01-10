7925e438c66554940869ca18892fb06b972cf24c
Andrew Pareles <andrewpareles@gmail.com>
2025-01-09 18:51:40 -0800

    header update

--- src/vs/workbench/contrib/void/browser/react/tailwind.config.js
+++ src/vs/workbench/contrib/void/browser/react/tailwind.config.js
@@ -1,7 +1,7 @@
-/*------------------------------------------------------------------------------------------
- *  Copyright (c) 2025 Glass Devtools, Inc. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.txt in the project root for more information.
- *-----------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------
+ *  Copyright 2025 Glass Devtools, Inc. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0. See LICENSE.txt for more information.
+ *--------------------------------------------------------------------------------------*/
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {

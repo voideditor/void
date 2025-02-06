323d80ef55d3487356d87f7ed23ee7123d4d25d9
Andrew Pareles <andrewpareles@gmail.com>
2025-02-06 03:02:46 -0800

    add divs for html

--- src/vs/workbench/contrib/void/browser/react/src/markdown/ChatMarkdownRender.tsx
+++ src/vs/workbench/contrib/void/browser/react/src/markdown/ChatMarkdownRender.tsx
@@ -193,11 +193,11 @@ const RenderToken = ({ token, nested = false, noSpace = false }: { token: Token 
 
 	if (t.type === "html") {
 		return (
-			<pre className={`bg-4oid-bg-2 p-4 rounded-lg ${noSpace ? '' : 'my-4'} font-mono text-sm`}>
-				{`<html>`}
+			<div className={`bg-4 bg-2 p-4 rounded-lg ${noSpace ? '' : 'my-4'} font-mono text-sm`}>
+				<div>{`<html>`}</div>
 				{t.raw}
-				{`</html>`}
-			</pre>
+				<div>{`</html>`}</div>
+			</div>
 		)
 	}
 

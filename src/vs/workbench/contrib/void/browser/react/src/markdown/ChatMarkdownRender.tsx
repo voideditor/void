826d148305ab15b52fd8de5717b35f1785344b54
Andrew Pareles <andrewpareles@gmail.com>
2025-02-06 03:03:14 -0800

    html

--- src/vs/workbench/contrib/void/browser/react/src/markdown/ChatMarkdownRender.tsx
+++ src/vs/workbench/contrib/void/browser/react/src/markdown/ChatMarkdownRender.tsx
@@ -193,11 +193,11 @@ const RenderToken = ({ token, nested = false, noSpace = false }: { token: Token 
 
 	if (t.type === "html") {
 		return (
-			<div className={`bg-4 bg-2 p-4 rounded-lg ${noSpace ? '' : 'my-4'} font-mono text-sm`}>
+			<pre className={`bg-void-bg-2 p-4 rounded-lg ${noSpace ? '' : 'my-4'} font-mono text-sm`}>
 				<div>{`<html>`}</div>
 				{t.raw}
 				<div>{`</html>`}</div>
-			</div>
+			</pre>
 		)
 	}
 

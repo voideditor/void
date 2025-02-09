9cbe83b57cc8a269fe10d58f382845435f2d946d
Andrew Pareles <andrewpareles@gmail.com>
2025-02-09 00:46:39 -0800

    comment

--- src/vs/workbench/contrib/void/browser/helpers/readFile.ts
+++ src/vs/workbench/contrib/void/browser/helpers/readFile.ts
@@ -3,7 +3,7 @@ import { EndOfLinePreference } from '../../../../../editor/common/model'
 import { IModelService } from '../../../../../editor/common/services/model.js'
 import { IFileService } from '../../../../../platform/files/common/files'
 
-// read files from VSCode (looks like only works if model exists already, use raw if not)
+// read files from VSCode. preferred (but appears to only work if the model of this URI already exists. If it doesn't use the other function.)
 export const VSReadFile = async (modelService: IModelService, uri: URI): Promise<string | null> => {
 	const model = modelService.getModel(uri)
 	if (!model) return null

28bf838b55c1a4e462659ccbfddfe0b67c458511
Joaquin Coromina <75667013+bjoaquinc@users.noreply.github.com>
2025-02-14 10:05:30 +0700

    Updated void-server endpoiont to make it dynamic

--- extensions/open-remote-ssh/src/serverSetup.ts
+++ extensions/open-remote-ssh/src/serverSetup.ts
@@ -39,7 +39,7 @@ export class ServerInstallError extends Error {
 	}
 }
 
-const DEFAULT_DOWNLOAD_URL_TEMPLATE = 'https://github.com/voideditor/${NAME_OF_REPO}/releases/download/${version}.${release}/void-server-${os}-${arch}-${version}.${release}.tar.gz';
+const DEFAULT_DOWNLOAD_URL_TEMPLATE = 'https://github.com/voideditor/void-updates-server/releases/download/test/void-server-${os}-${arch}.tar.gz';
 
 export async function installCodeServer(conn: SSHConnection, serverDownloadUrlTemplate: string | undefined, extensionIds: string[], envVariables: string[], platform: string | undefined, useSocketPath: boolean, logger: Log): Promise<ServerInstallResult> {
 	let shell = 'powershell';

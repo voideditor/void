aafe5563339b6a43ff610fc2d136d6a65fd2810d
Andrew Pareles <andrewpareles@gmail.com>
2025-05-13 20:00:39 -0700

    azure via AzureOpenAI

--- src/vs/workbench/contrib/void/electron-main/llmMessage/sendLLMMessage.impl.ts
+++ src/vs/workbench/contrib/void/electron-main/llmMessage/sendLLMMessage.impl.ts
@@ -7,7 +7,8 @@
 /* eslint-disable */
 import Anthropic from '@anthropic-ai/sdk';
 import { Ollama } from 'ollama';
-import OpenAI, { ClientOptions } from 'openai';
+import OpenAI, { ClientOptions, AzureOpenAI } from 'openai';
+import { getBearerTokenProvider, DefaultAzureCredential } from '@azure/identity';
 import { MistralCore } from '@mistralai/mistralai/core.js';
 import { fimComplete } from '@mistralai/mistralai/funcs/fimComplete.js';
 import { Tool as GeminiTool, FunctionDeclaration, GoogleGenAI, ThinkingConfig, Schema, Type } from '@google/genai';
@@ -114,9 +115,11 @@ const newOpenAICompatibleSDK = async ({ settingsOfProvider, providerName, includ
 	}
 	else if (providerName === 'microsoftAzure') {
 		// https://learn.microsoft.com/en-us/rest/api/aifoundry/model-inference/get-chat-completions/get-chat-completions?view=rest-aifoundry-model-inference-2024-05-01-preview&tabs=HTTP
+		const credential = new DefaultAzureCredential()
 		const thisConfig = settingsOfProvider[providerName]
-		const baseURL = `https://${thisConfig.project}.services.ai.azure.com/api/models/chat/completions?api-version=${thisConfig.azureApiVersion}`
-		return new OpenAI({ baseURL: baseURL, apiKey: thisConfig.apiKey, ...commonPayloadOpts })
+		const scope = thisConfig.project
+		const azureADTokenProvider = getBearerTokenProvider(credential, scope)
+		return new AzureOpenAI({ azureADTokenProvider, apiVersion: thisConfig.azureApiVersion })
 	}
 
 	else if (providerName === 'deepseek') {
